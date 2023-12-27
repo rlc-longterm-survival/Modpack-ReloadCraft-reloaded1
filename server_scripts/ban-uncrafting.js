a5890d4c48c9edbfd2649c64376656126115ba2a
yezhiyi9670 <yezhiyi0562@foxmail.com>
2023-12-27 15:55:04 +0800

    Add uncrafting table functionality

--- server_scripts/ban-uncrafting.js
+++ server_scripts/ban-uncrafting.js
@@ -4,4 +4,26 @@
       event.cancel();
     }
   })
+  ServerEvents.tick(event => {
+    const server = event.server
+    const players = server.getPlayerList().getPlayers()
+    for(let player of players) {
+      let level = player.getLevel()
+      let coord = [ player.getX(), player.getY(), player.getZ() ]
+      if(coord[1] - Math.floor(coord[1]) >= 0.5) {
+        return
+      }
+      let blockpos = [ Math.floor(coord[0]), Math.floor(coord[1]), Math.floor(coord[2]) ]
+      let block = level.getBlock(blockpos[0], blockpos[1] - 1, blockpos[2])
+      if(block.getId() != 'twilightforest:uncrafting_table') {
+        return
+      }
+      let jumpboostEffect = player.getEffect('minecraft:jump_boost')
+      if(jumpboostEffect && jumpboostEffect.duration >= 10) {
+        return
+      }
+      player.unlockAdvancement('twilightforest:uncraft_uncrafting_table');
+      server.runCommandSilent('effect give ' + player.username + ' minecraft:jump_boost 1 63 false')
+    }
+  })
 })()
