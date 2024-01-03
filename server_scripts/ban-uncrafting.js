34b7427982e487d9e64bcfe97bbb456a2b70f76c
yezhiyi9670 <yezhiyi0562@foxmail.com>
2024-01-04 01:52:38 +0800

    Fixed uncrafting table

--- server_scripts/ban-uncrafting.js
+++ server_scripts/ban-uncrafting.js
@@ -11,16 +11,16 @@
       let level = player.getLevel()
       let coord = [ player.getX(), player.getY(), player.getZ() ]
       if(coord[1] - Math.floor(coord[1]) >= 0.5) {
-        return
+        continue
       }
       let blockpos = [ Math.floor(coord[0]), Math.floor(coord[1]), Math.floor(coord[2]) ]
       let block = level.getBlock(blockpos[0], blockpos[1] - 1, blockpos[2])
       if(block.getId() != 'twilightforest:uncrafting_table') {
-        return
+        continue
       }
       let jumpboostEffect = player.getEffect('minecraft:jump_boost')
       if(jumpboostEffect && jumpboostEffect.duration >= 10) {
-        return
+        continue
       }
       player.unlockAdvancement('twilightforest:uncraft_uncrafting_table');
       server.runCommandSilent('effect give ' + player.username + ' minecraft:jump_boost 1 63 false')
